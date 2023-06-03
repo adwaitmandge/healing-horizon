f37a8a6c716184aadca62f5873e6a66016970bb2
Firefist01 <adwaitmandge@gmail.com>
2023-06-03 13:09:00 +0530

    First Commit

---
+++ client/src/components/Header.js
@@ -0,0 +1,120 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+
+const Header = () => {
+  const [top, setTop] = useState(true);
+
+  const { data: session } = useSession();
+  console.log(session);
+
+  const registerUser = async () => {
+    const userInfo = session.user;
+
+    try {
+      const res = await fetch("http://localhost:4000/api/user/register", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(userInfo),
+      });
+
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  // detect whether user has scrolled the page down by 10px
+  useEffect(() => {
+    const scrollHandler = () => {
+      window.pageYOffset > 10 ? setTop(false) : setTop(true);
+    };
+    window.addEventListener("scroll", scrollHandler);
+    return () => window.removeEventListener("scroll", scrollHandler);
+  }, [top, session]);
+
+  useEffect(() => {
+    if (session?.user) {
+      registerUser();
+    }
+  }, [session]);
+
+  return (
+    <header
+      className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${
+        !top && "bg-white backdrop-blur-sm shadow-lg"
+      }`}
+    >
+      <div className="max-w-6xl mx-auto px-5 sm:px-6">
+        <div className="flex items-center justify-between h-16 md:h-20">
+          {/* Site branding */}
+          <div className="flex-shrink-0 mr-4">
+            {/* Logo */}
+            <Link href="/" className="block" aria-label="Cruip">
+              <svg
+                className="w-8 h-8"
+                viewBox="0 0 32 32"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <defs>
+                  <radialGradient
+                    cx="21.152%"
+                    cy="86.063%"
+                    fx="21.152%"
+                    fy="86.063%"
+                    r="79.941%"
+                    id="header-logo"
+                  >
+                    <stop stopColor="#4FD1C5" offset="0%" />
+                    <stop stopColor="#81E6D9" offset="25.871%" />
+                    <stop stopColor="#338CF5" offset="100%" />
+                  </radialGradient>
+                </defs>
+                <rect
+                  width="32"
+                  height="32"
+                  rx="16"
+                  fill="url(#header-logo)"
+                  fillRule="nonzero"
+                />
+              </svg>
+            </Link>
+          </div>
+
+          {/* Site navigation */}
+          <nav className="flex flex-grow">
+            <ul className="flex flex-grow justify-end flex-wrap items-center">
+              <Link href="/">
+                <span className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">
+                  Home
+                </span>
+              </Link>
+              <li
+                href="/signup"
+                onClick={!session ? signIn : signOut}
+                className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3"
+              >
+                <span>{!session ? "Sign In" : "Sign Out"}</span>
+                <svg
+                  className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1"
+                  viewBox="0 0 12 12"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
+                    fillRule="nonzero"
+                  />
+                </svg>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
