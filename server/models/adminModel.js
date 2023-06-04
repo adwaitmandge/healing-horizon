c5d3426b8be7e9e3d0abf5ba37e41de0c9555361
Firefist01 <adwaitmandge@gmail.com>
2023-06-04 13:09:10 +0530

    Added middleware

--- server/models/adminModel.js
+++ server/models/adminModel.js
@@ -5,6 +5,15 @@ const adminSchema = mongoose.Schema({
   email: { type: String, required: true },
 });
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const Admin = mongoose.Model("User", adminSchema);
 
 module.exports = { Admin };
--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,3 +1,15 @@
-const router = require("express").Router();
+const express = require("express");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/userControllers");
+const { protect } = require("../middleware/authMiddleware");
 
-module.exports = { router };
+const router = express.Router();
+
+router.route("/").get(protect, allUsers);
+router.route("/").post(registerUser);
+router.post("/login", authUser);
+
+module.exports = router;
